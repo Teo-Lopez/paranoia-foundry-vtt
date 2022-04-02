bd911633c13a49a75e14b1737ed3eca2f3c251b8
DESKTOP-QBVEM9S\Teo <emaildeteo@gmail.com>
2022-04-03 01:28:19 +0200

    change code style

--- module/documents/item.mjs
+++ module/documents/item.mjs
@@ -1,3 +1,4 @@
+import { damageTable } from '../constants/damageTable'
 /**
  * Extend the basic Item with some very simple modifications.
  * @extends {Item}
@@ -9,20 +10,20 @@ export class ParanoiaItem extends Item {
   prepareData() {
     // As with the actor class, items are documents that can have their data
     // preparation methods overridden (such as prepareBaseData()).
-    super.prepareData();
+    super.prepareData()
   }
 
   /**
    * Prepare a data object which is passed to any Roll formulas which are created related to this Item
    * @private
    */
-   getRollData() {
+  getRollData() {
     // If present, return the actor's roll data.
-    if ( !this.actor ) return null;
-    const rollData = this.actor.getRollData();
-    rollData.item = foundry.utils.deepClone(this.data.data);
+    if (!this.actor) return null
+    const rollData = this.actor.getRollData()
+    rollData.item = foundry.utils.deepClone(this.data.data)
 
-    return rollData;
+    return rollData
   }
 
   /**
@@ -31,12 +32,12 @@ export class ParanoiaItem extends Item {
    * @private
    */
   async roll() {
-    const item = this.data;
+    const item = this.data
 
     // Initialize chat data.
-    const speaker = ChatMessage.getSpeaker({ actor: this.actor });
-    const rollMode = game.settings.get('core', 'rollMode');
-    const label = `[${item.type}] ${item.name}`;
+    const speaker = ChatMessage.getSpeaker({ actor: this.actor })
+    const rollMode = game.settings.get('core', 'rollMode')
+    const label = `[${item.type}] ${item.name}`
 
     // If there's no roll data, send a chat message.
     if (!this.data.data.formula) {
@@ -44,24 +45,24 @@ export class ParanoiaItem extends Item {
         speaker: speaker,
         rollMode: rollMode,
         flavor: label,
-        content: item.data.description ?? ''
-      });
+        content: item.data.description ?? '',
+      })
     }
     // Otherwise, create a roll and send a chat message from it.
     else {
       // Retrieve roll data.
-      const rollData = this.getRollData();
+      const rollData = this.getRollData()
 
       // Invoke the roll and submit it to chat.
-      const roll = new Roll(rollData.item.formula, rollData);
+      const roll = new Roll(rollData.item.formula, rollData)
       // If you need to store the value first, uncomment the next line.
       // let result = await roll.roll({async: true});
       roll.toMessage({
         speaker: speaker,
         rollMode: rollMode,
         flavor: label,
-      });
-      return roll;
+      })
+      return roll
     }
   }
 }
