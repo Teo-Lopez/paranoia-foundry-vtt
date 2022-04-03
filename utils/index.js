f0c0fc1f82ec1a519c0d355e1968139c8074299e
DESKTOP-QBVEM9S\Teo <emaildeteo@gmail.com>
2022-04-03 13:11:13 +0200

    add rolling for damage ask for armor

--- utils/index.js
+++ utils/index.js
@@ -1,2 +1,21 @@
 export const isItem = (item) => item.type === 'item'
 export const isWeapon = (item) => item.type === 'weapon'
+
+export const showDialog = ({
+  title,
+  content,
+  buttons,
+  default: defaultValue,
+}) => {
+  let d = new Dialog({
+    title,
+    content,
+    buttons,
+    default: defaultValue,
+    render: (html) =>
+      console.log('Register interactivity in the rendered dialog'),
+    close: (html) =>
+      console.log('This always is logged no matter which option is chosen'),
+  })
+  d.render(true)
+}
